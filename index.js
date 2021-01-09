31be97276f0cda07158e562c94a6ccd8c7d08456
Kamil Szymański <kszym94@gmail.com>
2021-01-09 20:21:54 +0100

    next

---
+++ index.js
@@ -0,0 +1,52 @@
+//jquery method
+$(document).ready(function(){
+
+//technology owl carousel
+
+$('#tech .owl-carousel').owlCarousel({
+    margin: 40,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        600:{
+
+            items:2
+        },
+
+        1000:{
+            items:3
+            }
+    }
+   
+});
+
+
+//security owl carousel
+$('#security .owl-carousel').owlCarousel({
+    margin: 40,
+    dots:false,
+    nav:true,
+    navText: ["<span><i class='fas fa-arrow-left'></i> Previous</span>","<span>Next<i class='fas fa-arrow-right'></i></span>"],
+    responsive:{
+        0:{
+            items:1
+        },
+
+        600:{
+
+            items:2
+        },
+
+        1000:{
+            items:3
+            }
+    }
+   
+});
+
+
+});
